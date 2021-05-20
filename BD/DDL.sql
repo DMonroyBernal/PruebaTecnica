a6d76e665c101dd950692d3840020d5e793506db
Ryuu2109 <66760174+Ryuu2109@users.noreply.github.com>
2021-05-20 00:28:00 -0500

    Add files via upload

---
+++ BD/DDL.sql
@@ -0,0 +1,12 @@
+create table Equipo(
+idEquipo int not null auto_increment,
+nombreEquipo varchar(250) not null,
+primary key(idEquipo)
+);
+
+create table Grupo(
+idGrupo int not null auto_increment,
+nombreGrupo varchar(250) not null,
+primary key(idGrupo)
+);
+
